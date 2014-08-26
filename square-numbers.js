57f4f00be87022b7a31a428cc048048da4665d75
Matias Leidemer <matiasleidemer@gmail.com>
2014-08-25 23:15:43 -0300

    created squareNumber module

---
+++ square-numbers.js
@@ -0,0 +1,9 @@
+//Require the underscore npm module
+var _ = require('underscore');
+
+function squareNumbers(list) {
+  return _.map(list, function(n) { return n*n });
+}
+
+//export squareNumbers as the module function
+module.exports = squareNumbers
